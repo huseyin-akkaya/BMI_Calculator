7061efdae8d2b4da83e47a1773a087b2f1f4bca3
Hüseyin Akkaya <huseyinakkaya721@gmail.com>
2025-09-09 09:48:43 +0300

    Update BMI__Calculator.py

--- BMI_Calculator/BMI__Calculator.py
+++ BMI_Calculator/BMI__Calculator.py
@@ -2,39 +2,39 @@ import tkinter
 
 
 window = tkinter.Tk()
-window.title("BMI Calculator")
-window.geometry("300x250")
+window.title("BMI Calculator") #uygulamanın üst kısmında adını yazdırır
+window.geometry("300x250") #açılacak pencerenin boyutu
 
 def calculate_process():
-    weight = input_one.get()
-    height = input_two.get()
+    weight = input_one.get() #işlenecek değerler çağırılır
+    height = input_two.get() #işlenecek değerler çağırılır
 
-    if weight == '' or height == '':
+    if weight == '' or height == '': #kullanıcı değer girmezse uyarı verir uygulama çökmemesi için
         result_label.config(text="Enter both weight and height!")
         return None
     else:
         try:
-            bmi = float(weight) / ((float(height) / 100) ** 2)
+            bmi = float(weight) / ((float(height) / 100) ** 2) #bmı hesaplanması
             return bmi
 
-        except:
+        except: #kullanıcı sayı yerine farklı değer girerse uyarı verir uygulama çökmesinin önüne geçebilmek için
             result_label.config(text="Enter a valid number!")
             return None
 
 
-input_question = tkinter.Label(text="Enter Your Wight(kg)",font=('Arial Black',10,'normal'))
+input_question = tkinter.Label(text="Enter Your Wight(kg)",font=('Arial Black',10,'normal')) #kullanıcıdan verilerin istenmesi
 input_question.pack(pady=10)
 
-input_one = tkinter.Entry(width=10)
+input_one = tkinter.Entry(width=10) #kullanıcı girdisi
 input_one.pack()
 
-input_question2 = tkinter.Label(text="Enter Your Height(cm)",font=('Arial Black',10,'normal'))
+input_question2 = tkinter.Label(text="Enter Your Height(cm)",font=('Arial Black',10,'normal')) #kullanıcıdan verilerin istennmesi
 input_question2.pack(pady=10)
 
-input_two = tkinter.Entry(width=10)
+input_two = tkinter.Entry(width=10) #kullanıcı girdisi
 input_two.pack()
 
-result_label = tkinter.Label(text="",font=('Arial',10,'normal'))
+result_label = tkinter.Label(text="",font=('Arial',10,'normal')) #ekranın altında bmı sonucun, kullanıcı kaynaklı hataların uyarılarının yazılması
 result_label.pack(pady=20)
 
 
@@ -44,7 +44,7 @@ def calculate_fonk():
     if result is None:  # sayı değilse veya boşsa direkt çık
         return
 
-    if 18.5<= result <= 24.9:
+    if 18.5<= result <= 24.9: #yapılan bmı hesabının uygun aralığa göre kullanıcıya yansıtılması
         result_label.config(text=f"Your BMI is: {result:.2f} -> Normal",fg="green")
     elif result<18.5:
         result_label.config(text=f"Your BMI is: {result:.2f} -> Weak",fg="blue")
@@ -59,8 +59,9 @@ def calculate_fonk():
 
 
 
-calculator_button = tkinter.Button(text="Calculate",command=calculate_fonk)
+calculator_button = tkinter.Button(text="Calculate",command=calculate_fonk) #hesaplama işlemini başlatabilmesi için kullanıcını tıklayacağı buton
 calculator_button.pack(pady=10)
+
 
 
 window.mainloop()
